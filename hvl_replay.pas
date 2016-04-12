35444c9652fdfa4d97e8b86ef269ddd4bc01a358
magorium <magorium@users.noreply.github.com>
2016-04-12 18:22:41 +0200

    Add hvl_GenSawtooth()

--- hvl_replay.pas
+++ hvl_replay.pas
@@ -271,4 +271,27 @@ begin
 end;
 
 
+
+procedure hvl_GenSawtooth(buf: pint8; len: uint32);
+var
+  i     : uint32;
+  val, 
+  add   : int32;
+begin
+  add := 256 div (len-1);
+  val := -128;
+
+  i := 0;
+  while (i < len) do 
+  begin
+    buf^ := int8(val);   
+    inc(buf, 1);
+    val  := val + add;
+
+    inc(i);
+  end;    
+end;
+
+
+
 end.

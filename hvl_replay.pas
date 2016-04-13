3f20a1aecbf1b7af28af5f8dd90722c7f89f60bf
magorium <magorium@users.noreply.github.com>
2016-04-13 15:08:24 +0200

    Add hvl_GenTriangle()

--- hvl_replay.pas
+++ hvl_replay.pas
@@ -294,4 +294,64 @@ end;
 
 
 
+procedure hvl_GenTriangle(buf: pint8; len: uint32);
+var
+  i              : uint32;
+  d1, d2, d4, d5 : int32;
+  val            : int32;
+  buf2           : pint8;
+  c              : int8;
+begin
+  d2  := len;
+  d5  := len shr 2;
+  d1  := 128 div d5;
+
+  d4  := -(d2 shr 1);
+  val := 0;
+  
+  i := 0;
+  while ( i < d5 ) do
+  begin
+    buf^ := val;  
+    inc(buf, 1);
+    val := val + d1;
+    inc(i);
+  end;
+  buf^ := $7f; 
+  inc(buf, 1);
+
+  if ( d5 <> 1 ) then
+  begin
+    val := 128;
+
+    i := 0;
+    while ( i < d5-1 ) do
+    begin
+      val := val - d1;
+      buf^ := val;  
+      inc(buf, 1);
+      inc(i);
+    end;
+  end;
+
+  buf2 := buf + d4;
+
+  i := 0;
+  while ( i < d5*2 ) do
+  begin
+    c := buf2^; 
+    inc(buf2, 1);
+
+    if ( c = $7f )
+    then c := int8($80)         // FPC: cast to suppress warning
+    else c := -c;
+
+    buf^ := c;  
+    inc(buf, 1);
+    inc(i);
+  end;
+end;
+
+
+
 end.
